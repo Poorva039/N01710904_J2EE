e83770241c0a3fec10163e392dd1148b21f31b6f
poorva039 <patelpoorva039@gmail.com>
2026-01-28 21:13:05 -0500

    Add Lab 3

---
+++ lab3/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/app/EmployeemanagementSystemApp.java
@@ -0,0 +1,10 @@
+package com.example.employeemanagementsystem.app;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class EmployeemanagementSystemApp extends Application {
+    // Scans for @Path-annotated classes in the package
+}
